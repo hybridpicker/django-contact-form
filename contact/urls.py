9fb91ac5f35401c0392e57cd84afc6253ebf9bfe
Lukas Schönsgibl <lukas@schoensgibl.com>
2019-07-15 16:31:44 +0200

    view to contact

--- contact/urls.py
+++ contact/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from form import views
 
 urlpatterns = [
-    path('contact/', views.emailView, name='contact_email'),
+    path('', views.emailView, name='contact_email'),
     path('contact/success/', views.successView, name='success_contact'),
 ]
