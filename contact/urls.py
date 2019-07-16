607b75445962141746e45fbcf7d94de8ec2c6b6f
Lukas <lschoensgibl@gmail.com>
2019-07-16 06:00:34 +0200

    contact form url to /contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from form import views
 
 urlpatterns = [
-    path('', views.emailView, name='contact_email'),
-    path('contact/success/', views.successView, name='success_contact'),
+    path('', include('form.urls')),
 ]
---
+++ form/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from form import views
+
+urlpatterns = [
+    path('contact', views.emailView, name='contact_email'),
+    path('contact/success/', views.successView, name='success_contact'),
+]
